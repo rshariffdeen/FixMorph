11c07027330becdec74cbe6ae94a3cdab3fbcec5
ridwan <rshariffdeen@gmail.com>
2018-05-04 11:51:56 +0800

    adding llvm stub code

---
+++ src/clone-detector.cpp
@@ -0,0 +1,59 @@
+#include "llvm/Pass.h"
+#include "llvm/IR/Function.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/IR/LegacyPassManager.h"
+#include "llvm/IR/InstrTypes.h"
+#include "llvm/Transforms/IPO/PassManagerBuilder.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/Transforms/Utils/BasicBlockUtils.h"
+using namespace llvm;
+
+namespace {
+  struct SkeletonPass : public FunctionPass {
+    static char ID;
+    SkeletonPass() : FunctionPass(ID) {}
+
+    virtual bool runOnFunction(Function &F) {
+      for (auto &B : F) {
+        for (auto &I : B) {
+          if (auto *op = dyn_cast<BinaryOperator>(&I)) {
+            // Insert at the point where the instruction `op` appears.
+            IRBuilder<> builder(op);
+
+            // Make a multiply with the same operands as `op`.
+
+            Value *lhs = op->getOperand(0);
+            Value *rhs = op->getOperand(1);
+            Value *mul = builder.CreateMul(lhs, rhs);
+
+            errs() << "I found two variables " << lhs->getName().str() << " and " << rhs->getName().str() << "!\n";
+
+            // Everywhere the old instruction was used as an operand, use our
+            // new multiply instruction instead.
+            for (auto &U : op->uses()) {
+              User *user = U.getUser();  // A User is anything with operands.
+              user->setOperand(U.getOperandNo(), mul);
+            }
+
+            // We modified the code.
+            return true;
+          }
+        }
+      }
+
+      return false;
+    }
+  };
+}
+
+char SkeletonPass::ID = 0;
+
+// Automatically enable the pass.
+// http://adriansampson.net/blog/clangpass.html
+static void registerSkeletonPass(const PassManagerBuilder &,
+                         legacy::PassManagerBase &PM) {
+  PM.add(new SkeletonPass());
+}
+static RegisterStandardPasses
+  RegisterMyPass(PassManagerBuilder::EP_EarlyAsPossible,
+                 registerSkeletonPass);
