5dbc8e6e797b40eaa91c9fa2c7cdf8f37969f53f
ridwan <rshariffdeen@gmail.com>
2018-05-04 11:46:12 +0800

    adding sample test cases

---
+++ samples/prog-a.c
@@ -0,0 +1,19 @@
+#include <assert.h>
+#include <stdio.h>
+
+long int add (int x, int y)
+{
+    return x + y;
+}
+
+int main(void)
+{
+    int a, b, c = 7;
+
+    while (scanf("%d", &a) > 0) {
+        assert(a > 0);
+        printf("fact: %lu\n", add(a,c));
+    }
+
+    return 0;
+}
---
+++ samples/prog-c.c
@@ -0,0 +1,16 @@
+#include <assert.h>
+#include <stdio.h>
+
+long int sum (int a, int b)
+{
+    return a + b ;
+}
+
+int main(void)
+{
+    int i, b=j, k = 7;
+    scanf("%d", &i);
+    printf("fact: %lu\n", add(i,k));
+
+    return 0;
+}
