5790bd9f61d255bd25709e906a5e45bee946d84c
ridwan <rshariffdeen@gmail.com>
2018-05-15 19:22:47 +0800

    writing line-number-pass

--- pass/line-number-pass/CrochetLineNumberPass.cpp
+++ pass/line-number-pass/CrochetLineNumberPass.cpp
@@ -6,40 +6,63 @@
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
 #include "llvm/IR/IRBuilder.h"
 #include "llvm/Transforms/Utils/BasicBlockUtils.h"
+#include "llvm/IR/DebugInfoMetadata.h"
 using namespace llvm;
 
 namespace {
+
+  class CrochetFunction {
+      int first, last;
+  public:
+
+      void setValues(int x, int y){
+          first = x;
+          last = y;
+      }
+
+      int getFirst(){
+          return first;
+      }
+
+      int getLast(){
+          return last;
+      }
+
+
+  };
+
   struct CrochetLineNumberPass : public FunctionPass {
     static char ID;
       CrochetLineNumberPass() : FunctionPass(ID) {}
-
+      std::map<std::string, CrochetFunction> functionRange;
     virtual bool runOnFunction(Function &F) {
-      for (auto &B : F) {
-        for (auto &I : B) {
-          if (auto *op = dyn_cast<BinaryOperator>(&I)) {
-            // Insert at the point where the instruction `op` appears.
-            IRBuilder<> builder(op);
+        errs() << "In a function called " << F.getName() << "!\n";
+        bool firstInstruction = true;
+        unsigned firstLineNumber = 0;
+        unsigned lastLineNumber = 0;
 
-            // Make a multiply with the same operands as `op`.
 
-            Value *lhs = op->getOperand(0);
-            Value *rhs = op->getOperand(1);
-            Value *mul = builder.CreateMul(lhs, rhs);
+        for (auto &B : F) {
+            for (auto &I : B) {
+                if (firstInstruction){
+                    if (DILocation *Loc = I.getDebugLoc()) { // Here I is an LLVM instruction
+                        firstInstruction = false;
+                        firstLineNumber = Loc->getLine();
+                    }
+                }
 
-            errs() << "I found two variables " << lhs->getName().str() << " and " << rhs->getName().str() << "!\n";
+                if (isa<ReturnInst>(I)){
+                    if (DILocation *Loc = I.getDebugLoc()) { // Here I is an LLVM instruction
+                        lastLineNumber = Loc->getLine();
+                    }
+                }
 
-            // Everywhere the old instruction was used as an operand, use our
-            // new multiply instruction instead.
-            for (auto &U : op->uses()) {
-              User *user = U.getUser();  // A User is anything with operands.
-              user->setOperand(U.getOperandNo(), mul);
             }
-
-            // We modified the code.
-            return true;
-          }
         }
-      }
+
+        CrochetFunction functionInfo;
+        functionInfo.setValues(firstLineNumber,lastLineNumber);
+        functionRange[F.getName()] = functionInfo ;
 
       return false;
     }
