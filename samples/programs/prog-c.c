6a4f75b742ecf926250619404bb2c7bca032b058
ridwan <rshariffdeen@gmail.com>
2018-05-15 11:48:40 +0800

    changing variable names

--- samples/programs/prog-c.c
+++ samples/programs/prog-c.c
@@ -7,23 +7,23 @@ long sum (int a, int b)
     return a + b ;
 }
 
-int procedure (int i, int j){
-    int k = 10;
+int procedure (int x, int y){
+    int z = 10;
 
     clock_t start  = clock();
     clock_t finish;
 
-    while (i < k){
-        i++;
+    while (x < z){
+        x++;
     }
 
     finish = clock();
     printf("look took :%fms\n", (double)finish-start/CLOCKS_PER_SEC);
 
-    j = 2*k;
+    y = 2*z;
 
-    printf("i=%d, j=%d",i,j);
-    return j;
+    printf("i=%d, j=%d",x,y);
+    return y;
 }
 
 int main(void)
