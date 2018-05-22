8865d6fa86c547acef5cc42de27175a41a88a780
Pedro Bahamondes <pibahamondes@uc.cl>
2018-05-22 11:12:24 +0800

    Changes to crochet.py for compatibility and simplification

--- crochet.py
+++ crochet.py
@@ -5,44 +5,22 @@ import subprocess as sub
 project_A = dict()
 project_B = dict()
 project_C = dict()
+proj = [project_A, project_B, project_C]
 diff_info = dict()
 code_clones = dict()
 
 
-
-
 def load_projects():
-    source_path_a = sys.argv[1]
-    source_path_b = sys.argv[2]
-    source_path_c = sys.argv[3]
-
-    if not os.path.isdir(source_path_a):
-        print "source directory A not found"
-        print source_path_a
-        exit(-1)
-    else:
-        project_A["dir_path"] = os.path.abspath(source_path_a) + "/"
-        project_A["dir_name"] = os.path.abspath(source_path_a).split("/")[-1]
-        project_A["output_dir"] = "output/" + project_A["dir_name"]
-
-    if not os.path.isdir(source_path_b):
-        print "source directory B not found"
-        print source_path_b
-        exit(-1)
-    else:
-        project_B["dir_path"] = os.path.abspath(source_path_b) + "/"
-        project_B["dir_name"] = os.path.abspath(source_path_b).split("/")[-1]
-        project_B["output_dir"] = "output/" + project_B["dir_name"]
-
-    if not os.path.isdir(source_path_c):
-        print "source directory C not found"
-        print source_path_c
-        exit(-1)
-    else:
-        project_C["dir_path"] = os.path.abspath(source_path_c) + "/"
-        project_C["dir_name"] = os.path.abspath(source_path_c).split("/")[-1]
-        project_C["output_dir"] = "output/" + project_C["dir_name"]
-
+    for i in range(3):
+        source_path = sys.argv[i+1]
+        if not os.path.isdir(source_path):
+            print("source directory A not found:\n"+ source_path)
+            exit(-1)
+        else:
+            proj[i]["dir_path"] = os.path.abspath(source_path) + "/"
+            proj[i]["dir_name"] = os.path.abspath(source_path).split("/")[-1]
+            proj[i]["output_dir"] = "output/" + proj[i]["dir_name"]
+            
 
 def cmd_exec(command):
     p = sub.Popen([command], stdout=sub.PIPE, stderr=sub.PIPE)
@@ -54,24 +32,23 @@ def generate_line_range_per_function(source_file_path):
     command = "clang-7 -Wno-everything -g -Xclang -load -Xclang lib/libCrochetLineNumberPass.so " + source_file_path + " 2> function-range"
     os.system(command)
     function_range = dict()
-    with open('function-range') as range_file:
-        line = str(range_file.readline())
+    with open('function-range', 'r') as range_file:
+        line = range_file.readline().strip().split(":")
         while line:
-            print  "\n" + line + "\n"
-            function_name = line.split(":")[0]
-            start = line.split(":")[1].split("-")[0]
-            end = line.split(":")[1].split("-")[1]
+            function_name = line[0]
+            start = line[1].split("-")[0]
+            end = line[1].split("-")[1]
+            print(function_name, start, end)
             function_range[function_name] = dict()
             function_range[function_name]['start'] = int(start)
-            function_range[function_name]['end'] = int(end.replace("\n", ''))
-            line = str(range_file.readline())
+            function_range[function_name]['end'] = int(end)
+            line = range_file.readline().strip().split(":")
     return function_range
-
+    
 
 def generate_ast_dump(project):
     command = "clang -Xclang -ast-dump -fsyntax-only -fno-color-diagnostics " + string(
         source_file.filePath) + " | grep -P \"(Function|Var)Decl\" > " + output_path + "/" + "declarations.txt"
-
 
 
 def generate_variable_slices(project):
@@ -96,7 +73,7 @@ def generate_deckard_vectors(project):
 
 def get_diff_info():
     diff_file_list_command = "diff -qr " + project_A["dir_path"] + " " + project_B["dir_path"]
-    diff_file_list_command += " | grep '[a-zA-Z0-9].c ' > diff-files"
+    diff_file_list_command += " | grep -P '[A-Za-z0-9_].c ' > diff-files"
     os.system(diff_file_list_command)
     with open('diff-files') as diff_file:
         diff_file_path = str(diff_file.readline())
@@ -138,16 +115,16 @@ def create_output_directories():
 
 def run():
     if len(sys.argv) < 4:
-        print "Insufficient arguments"
+        print("Insufficient arguments")
         exit(-1)
 
     load_projects()
     get_diff_info()
 
     for file_name, function_list in diff_info.items():
-        print file_name + ":"
+        print(file_name + ":")
         for function_name, line_range in function_list.items():
-            print "\t" + function_name + " " + str(line_range['start']) + "-" + str(line_range['end'])
+            print("\t" + function_name + " " + str(line_range['start']) + "-" + str(line_range['end']))
 
     # create_output_directories()
     # generate_patch_slices()
