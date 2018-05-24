f854d454622717b9ffe95635fa7284e12eb11bb3
ridwan <rshariffdeen@gmail.com>
2018-05-24 18:08:29 +0800

    collecting variable information

--- CSParser.py
+++ CSParser.py
@@ -22,28 +22,35 @@ def create_output_directories():
         os.system("mkdir " + output_dir)
 
 
-def get_function_lines():
+def get_function_details():
     for func in project.procedures():
         function_name = func.name()
-        print function_name
         if '#' in function_name:
             continue
-
         try:
             source_file = func.file_line()
             source_file_path = source_file[0].name()
             if source_file_path not in function_info:
                 function_info[source_file_path] = dict()
 
-            start_line = func.entry_point().file_line()[1]
-            end_line = func.exit_point().file_line()[1]
-            line_range = dict()
-            line_range['start'] = str(start_line)
-            line_range['end'] = str(end_line)
-            function_info[source_file_path][function_name] = line_range
+            function_info[source_file_path][function_name] = dict()
+
+            function_info[source_file_path][function_name]['variable-list'] = get_variable_list(func)
+
+            function_info[source_file_path][function_name]['line-range'] = get_function_lines(func)
 
         except:
-            print "exception processing system function "
+            print "exception processing system function ", function_name
+            continue
+
+
+def get_function_lines(procedure):
+    start_line = procedure.entry_point().file_line()[1]
+    end_line = procedure.exit_point().file_line()[1]
+    line_range = dict()
+    line_range['start'] = str(start_line)
+    line_range['end'] = str(end_line)
+    return line_range
 
 
 def print_function_lines():
@@ -60,14 +67,53 @@ def kill_csruf_shell():
     os.kill(int(pid), signal.SIGKILL)
 
 
+def get_variable_line_number_list (line_list, source_file_name):
+    line_number_list = list()
+    for line in line_list:
+        try:
+            file_name = line.file_line()[0].name()
+            if file_name == source_file_name:
+                line_number = line.file_line()[1]
+                if line_number not in line_number_list:
+                    line_number_list.append(line_number)
+
+        except:
+            #print "cannot determine line for variable "
+            continue
+    return line_number_list
+
+
+def get_variable_list(procedure):
+    symbol_list = list(procedure.local_symbols())
+    var_list = dict()
+    source_file_name = procedure.file_line()[0].name()
+
+    for var in symbol_list:
+        var_info = dict()
+        var_name = var.name().split("-")[0]
+
+        var_info['type'] = var.get_type().get_class().name()
+        declaration_lines = get_variable_line_number_list(var.declarations(), source_file_name)
+        var_info['dec-line-numbers'] = declaration_lines
+
+        used_lines = get_variable_line_number_list(list(var.used_points()), source_file_name)
+        var_info['use-line-numbers'] = used_lines
+
+        successive_lines = get_variable_line_number_list(list(var.used_points()), source_file_name)
+        killed_lines = get_variable_line_number_list(list(var.killed_points()) + list(var.may_killed_points()), source_file_name)
+        var_info['killed-line-numbers'] = killed_lines
+
+        var_list[var_name] = var_info
+    return var_list
+
+
 def run():
     initialize_project()
     create_output_directories()
-    get_function_lines()
+    get_function_details()
     print_function_lines()
-    #kill_csruf_shell()
+    # kill_csruf_shell()
     return
 
 
 run()
-
