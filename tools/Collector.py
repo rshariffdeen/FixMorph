cb5777869ce0c9ca90c9e0734673d6d99172a5e5
rshariffdeen <rshariffdeen@gmail.com>
2019-12-26 16:40:46 +0800

    move collect instructions to collector

--- tools/Collector.py
+++ tools/Collector.py
@@ -6,6 +6,83 @@ import sys
 import os
 from tools import Emitter, Logger
 import collections
+from common import Definitions
+from common.Utilities import error_exit, clean_parse
+
+
+def collect_instruction_list(script_file_path):
+    instruction_list = list()
+    inserted_node_list = list()
+    map_ab = dict()
+    with open(script_file_path, 'r') as script:
+        line = script.readline().strip()
+        while line:
+            line = line.split(" ")
+            # Special case: Update and Move nodeA into nodeB2
+            if len(line) > 3 and line[0] == Definitions.UPDATE and line[1] == Definitions.AND and \
+                    line[2] == Definitions.MOVE:
+                instruction = Definitions.UPDATEMOVE
+                content = " ".join(line[3:])
+
+            else:
+                instruction = line[0]
+                content = " ".join(line[1:])
+            # Match nodeA to nodeB
+            if instruction == Definitions.MATCH:
+                try:
+                    node_a, node_b = clean_parse(content, Definitions.TO)
+                    map_ab[node_b] = node_a
+                except Exception as e:
+                    error_exit(e, "Something went wrong in MATCH (AB).", line, instruction, content)
+            # Update nodeA to nodeB (only care about value)
+            elif instruction == Definitions.UPDATE:
+                try:
+                    node_a, node_b = clean_parse(content, Definitions.TO)
+                    if "TypeLoc" in node_a:
+                        continue
+                    instruction_list.append((instruction, node_a, node_b))
+                except Exception as e:
+                    error_exit(e, "Something went wrong in UPDATE.")
+            # Delete nodeA
+            elif instruction == Definitions.DELETE:
+                try:
+                    node_a = content
+                    instruction_list.append((instruction, node_a))
+                except Exception as e:
+                    error_exit(e, "Something went wrong in DELETE.")
+            # Move nodeA into nodeB at pos
+            elif instruction == Definitions.MOVE:
+                try:
+                    node_a, node_b = clean_parse(content, Definitions.INTO)
+                    node_b_at = node_b.split(Definitions.AT)
+                    node_b = Definitions.AT.join(node_b_at[:-1])
+                    pos = node_b_at[-1]
+                    instruction_list.append((instruction, node_a, node_b, pos))
+                except Exception as e:
+                    error_exit(e, "Something went wrong in MOVE.")
+            # Update nodeA into matching node in B and move into nodeB at pos
+            elif instruction == Definitions.UPDATEMOVE:
+                try:
+                    node_a, node_b = clean_parse(content, Definitions.INTO)
+                    node_b_at = node_b.split(Definitions.AT)
+                    node_b = Definitions.AT.join(node_b_at[:-1])
+                    pos = node_b_at[-1]
+                    instruction_list.append((instruction, node_a, node_b, pos))
+                except Exception as e:
+                    error_exit(e, "Something went wrong in MOVE.")
+                    # Insert nodeB1 into nodeB2 at pos
+            elif instruction == Definitions.INSERT:
+                try:
+                    node_a, node_b = clean_parse(content, Definitions.INTO)
+                    node_b_at = node_b.split(Definitions.AT)
+                    node_b = Definitions.AT.join(node_b_at[:-1])
+                    pos = node_b_at[-1]
+                    instruction_list.append((instruction, node_a, node_b, pos))
+                    inserted_node_list.append(node_a)
+                except Exception as e:
+                    error_exit(e, "Something went wrong in INSERT.")
+            line = script.readline().strip()
+    return instruction_list, inserted_node_list, map_ab
 
 
 def collect_symbolic_expressions(trace_file_path):
