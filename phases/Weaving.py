23fb8194242f82c84dbb9eb4e0f5d943e101761a
rshariffdeen <rshariffdeen@gmail.com>
2019-12-30 10:58:01 +0800

    fix list conversion error

--- phases/Weaving.py
+++ phases/Weaving.py
@@ -31,7 +31,7 @@ def load_values():
         script_info = dict()
         script_list = Reader.read_json(Definitions.FILE_SCRIPT_INFO)
         for (path_info, trans_script_info) in script_list:
-            script_info[(path_info[0], path_info[1], path_info[2])] = trans_script_info
+            script_info[(path_info[0], path_info[1], path_info[2])] = trans_script_info[0]
         Values.translated_script_for_files = script_info
 
     # Definitions.FILE_SCRIPT_INFO = Definitions.DIRECTORY_OUTPUT + "/script-info"
