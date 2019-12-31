9d5178ea941de6cffceb14a07754aaacbc8ec326
rshariffdeen <rshariffdeen@gmail.com>
2019-12-31 14:19:16 +0800

    adding driver

---
+++ experiments/patchweave/driver.py
@@ -0,0 +1,17 @@
+KEY_CATEGORY = "category"
+KEY_DONOR = "donor"
+KEY_TARGET = "target"
+KEY_BUG_NAME = "bug_name"
+KEY_POC_PATH = "poc"
+KEY_CVE_ID = "cve"
+
+
+TOOL_PATH = ""
+
+
+def setup():
+    print("\tcreating setup")
+
+
+def evaluate():
+    print("\trunning evaluation")
