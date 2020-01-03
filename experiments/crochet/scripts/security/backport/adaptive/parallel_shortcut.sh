ff5affa5ef46ee00456da08b4b5fe20e4b7c6b60
linxu <A0067104@gmail.com>
2020-01-03 09:20:33 +0800

    New logs

--- experiments/crochet/scripts/security/backport/adaptive/parallel_shortcut.sh
+++ experiments/crochet/scripts/security/backport/adaptive/parallel_shortcut.sh
@@ -1,6 +1,6 @@
 nohup ./CVE-2017-0381-Android.sh &
 nohup ./CVE-2017-0381-Firefox.sh &
 nohup ./CVE-2017-0381-PaleMoon.sh &
-#nohup ./CVE-2017-12087.sh &
-#nohup ./CVE-2017-3735.sh &
+nohup ./CVE-2017-12087.sh &
+nohup ./CVE-2017-3735.sh &
 #nohup ./CVE-2018-14526.sh &
