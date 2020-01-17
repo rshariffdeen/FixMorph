03c233d32c3837f46f32e1ceede57fbb009d1c8f
linxu <A0067104@gmail.com>
2020-01-17 16:16:01 +0800

    New results after hash modification

--- experiments/crochet/scripts/security/backport/adaptive/parallel_shortcut.sh
+++ experiments/crochet/scripts/security/backport/adaptive/parallel_shortcut.sh
@@ -1,6 +1,8 @@
-nohup ./CVE-2017-0381-Android.sh &
+nohup ./CVE-2018-14526_11_1.sh &
 #nohup ./CVE-2017-0381-Firefox.sh &
-nohup ./CVE-2017-0381-PaleMoon.sh &
-nohup ./CVE-2017-12087.sh &
+nohup ./CVE-2018-14526_10_4.sh &
+nohup ./CVE-2017-13077_11_1.sh &
+nohup ./CVE-2017-13077_10_4.sh &
+nohup ./CVE-2017-13077_Android.sh &
 #nohup ./CVE-2017-3735.sh &
 #nohup ./CVE-2018-14526.sh &
