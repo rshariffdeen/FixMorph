935f97cda20336663da1e7d6ef591938542e0d5d
rshariffdeen <rshariffdeen@gmail.com>
2020-01-31 16:29:15 +0800

    add only functions in file

--- tools/Generator.py
+++ tools/Generator.py
@@ -44,7 +44,8 @@ def generate_vectors(file_extension, log_file, project):
                     elif node_type in ["TypedefDecl"]:
                         type_def_list.append((ast_node["value"], ast_node["start line"], ast_node["end line"]))
                     elif node_type in ["RecordDecl"]:
-                        struct_list.append((ast_node["value"], ast_node["start line"], ast_node["end line"]))
+                        if ast_node['file'] == source_file:
+                            struct_list.append((ast_node["value"], ast_node["start line"], ast_node["end line"]))
                     elif node_type in ["FunctionDecl"]:
                         if ast_node['file'] == source_file:
                             function_list.append((ast_node["value"], ast_node["start line"], ast_node["end line"]))
