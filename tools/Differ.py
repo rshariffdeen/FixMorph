f6405d6cfe46200864b53c882bc07f3abf426da8
rshariffdeen <rshariffdeen@gmail.com>
2020-02-11 15:32:18 +0800

    append filtered file

--- tools/Differ.py
+++ tools/Differ.py
@@ -60,6 +60,7 @@ def diff_h_files(diff_file_path, project_path_a, untracked_file_list):
             file_a = h_file[0]
             file_a_rel_path = file_a.replace(project_path_a, "")[1:]
             if file_a_rel_path not in untracked_file_list:
+                filtered_file_list.append(h_file)
                 Emitter.normal("\t\t\t" + file_a)
     else:
         Emitter.normal("\t\t\t-none-")
@@ -82,10 +83,11 @@ def diff_c_files(diff_file_path, project_path_a, untracked_file_list):
             diff_line = diff_file.readline().strip()
     Emitter.normal("\t\tsource files:")
     if len(file_list) > 0:
-        for h_file in file_list:
-            file_a = h_file[0]
+        for c_file in file_list:
+            file_a = c_file[0]
             file_a_rel_path = file_a.replace(project_path_a, "")[1:]
             if file_a_rel_path not in untracked_file_list:
+                filtered_file_list.append(c_file)
                 Emitter.normal("\t\t\t" + file_a)
     else:
         Emitter.normal("\t\t\t-none-")
