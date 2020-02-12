8567d25ebd8ec66bd839d1b184b17ea698a68a75
rshariffdeen <rshariffdeen@gmail.com>
2020-02-12 12:17:27 +0800

    add script to scrape git info

--- scripts/git-info.py
+++ scripts/git-info.py
@@ -1,4 +1,4 @@
-import os
+import io
 import sys
 from git import Repo
 
@@ -9,7 +9,7 @@ KEY_VERSION = "version"
 
 repo_path = sys.argv[2]
 input_file_path = sys.argv[1]
-commit_info_list = dict()
+commit_info_list = list()
 commit_list = list()
 
 repo = Repo(repo_path)
@@ -35,18 +35,20 @@ for commit_id in commit_list:
     for diff in diff_list:
         file_list.append(diff.a_path)
 
-    tag_list = list()
-    tree_id = current_commit.tree.hexsha
-    for tag_ref in repo.tags:
-        if tag_ref.tag is not None:
-            if tag_ref.tag.hexsha == tree_id:
-                tag_list.append(tag_ref.name)
+    makefile = current_commit.tree / 'Makefile'
+    with io.BytesIO(makefile.data_stream.read()) as f:
+        content = f.readlines()
+
+    major_version = content[0].split(" = ")[1].replace("\n", '').strip()
+    minor_version = content[1].split(" = ")[1].replace("\n", '').strip()
+    sub_verison = content[2].split(" = ")[1].replace("\n", '').strip()
+    version = major_version + "." + minor_version + "." + sub_verison
 
     commit_info[KEY_COMMIT_ID] = commit_id
     commit_info[KEY_PARENT_COMMIT] = parent_commit_id
     commit_info[KEY_FILE_LIST] = file_list
-    commit_info[KEY_VERSION] = tag_list
-    commit_info_list[commit_id] = commit_info
+    commit_info[KEY_VERSION] = version
+    commit_info_list.append(commit_info)
 
 print(commit_info_list)
 
