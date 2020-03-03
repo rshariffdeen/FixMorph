74a96f49a72f27228a9dc1f49b29d80f6c17461a
rshariffdeen <rshariffdeen@gmail.com>
2020-03-03 15:47:00 +0800

    remove experiments from tool

--- experiments/crochet/old/scripts/security/backport/adaptive/parallel_shortcut.sh
+++
@@ -1,9 +0,0 @@
-#nohup ./CVE-2017-0381-Firefox.sh &
-nohup ./CVE-2017-13077_11_1.sh &
-nohup ./CVE-2017-13077_10_4.sh &
-nohup ./CVE-2017-13077_Android.sh &
-nohup ./CVE-2017-13088_11_1.sh &
-nohup ./CVE-2017-13088_10_4.sh &
-nohup ./CVE-2017-13088_Android.sh &
-#nohup ./CVE-2017-3735.sh &
-#nohup ./CVE-2018-14526.sh &
