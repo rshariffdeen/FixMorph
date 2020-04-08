e84b1b3acc8673492f0cd00bab911c45b2efe987
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2020-04-08 14:55:46 +0800

    return empty dict

--- tools/Reader.py
+++ tools/Reader.py
@@ -10,9 +10,10 @@ from tools import Logger
 
 def read_json(file_path):
     Logger.trace(__name__ + ":" + sys._getframe().f_code.co_name, locals())
+    json_data = dict()
     if os.path.isfile(file_path):
         with open(file_path, 'r') as in_file:
             content = in_file.readline()
             json_data = json.loads(content)
-            return json_data
+    return json_data
 
