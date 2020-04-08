8980022837d5b219fea3f54b9ece452746e212e3
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2020-04-08 14:47:35 +0800

    open only if file exists

--- tools/Reader.py
+++ tools/Reader.py
@@ -4,13 +4,15 @@
 
 import sys
 import json
+import os
 from tools import Logger
 
 
 def read_json(file_path):
     Logger.trace(__name__ + ":" + sys._getframe().f_code.co_name, locals())
-    with open(file_path, 'r') as in_file:
-        content = in_file.readline()
-        json_data = json.loads(content)
-        return json_data
+    if os.path.isfile(file_path):
+        with open(file_path, 'r') as in_file:
+            content = in_file.readline()
+            json_data = json.loads(content)
+            return json_data
 
