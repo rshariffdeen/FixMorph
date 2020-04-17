55c6017e5f62a9572aa99a2c5d69386e9d3ef8a7
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2020-04-18 00:48:11 +0800

    print diff info

--- phases/Comparison.py
+++ phases/Comparison.py
@@ -123,169 +123,28 @@ def safe_exec(function_def, title, *args):
     return result
 
 
-def get_source_line_numbers(path_a):
-    number_list = list()
-    if Values.PATH_A in path_a:
-        path_b = path_a.replace(Values.PATH_A, Values.PATH_B)
-    else:
-        path_b = path_a.replace(Values.PATH_C, Values.PATH_E)
-
-    file_a = open(path_a, "rt").readlines()
-    file_b = open(path_b, "rt").readlines()
-    differ = difflib.Differ()
-    diff_list = differ.compare(file_a, file_b)
-
-    # TODO: implement to get the changed line numbers
-
-    return number_list
-
-
-def get_ast_line_numbers(file_path):
-    number_list = list()
-    return number_list
-
-
-def get_ast_json(file_path):
-    json_file = file_path + ".AST"
-    dump_command = "crochet-diff -ast-dump-json " + file_path
-    if file_path[-1] == 'h':
-        dump_command += " --"
-    dump_command += " 2> output/errors_AST_dump > " + json_file
-    if os.stat(json_file).st_size == 0:
-        return None
-    with io.open(json_file, 'r', encoding='utf8', errors="ignore") as f:
-        ast_json = json.loads(f.read())
-    return ast_json['root']
-
-
-def get_variable_list(path_a):
-    var_list = list()
-    if Values.PATH_A in path_a:
-        path_b = path_a.replace(Values.PATH_A, Values.PATH_B)
-    else:
-        path_b = path_a.replace(Values.PATH_C, Values.PATH_E)
-
-    ast_script = get_ast_script(path_a, path_b)
-    ast_a = get_ast_json(path_a)
-    ast_b = get_ast_json(path_b)
-
-
-
-    return var_list
-
-
-def get_macro_list():
-    macro_list = list()
-    return macro_list
-
-
-def get_header_files():
-    file_list = list()
-    return file_list
-
-
-def get_function_call_list():
-    call_list = list()
-    return call_list
-
-
-def get_ast_script(source_a, source_b):
-    ast_script = list()
-    Logger.trace(__name__ + ":" + sys._getframe().f_code.co_name, locals())
-    generate_command = "crochet-diff "
-    generate_command += source_a + " " + source_b
-    if source_a[-1] == 'h':
-        generate_command += " --"
-    generate_command += " > " + Definitions.FILE_AST_SCRIPT
-    execute_command(generate_command, False)
-    with open(Definitions.FILE_AST_SCRIPT, "r") as script_file:
-        ast_script = script_file.readlines()
-    return ast_script
-
-
-def get_diff_files(project_path):
-    repo = Repo(project_path)
-    patch_commit = repo.head.commit
-    parent_commit = patch_commit.parents[0]
-    diff_list = parent_commit.diff(patch_commit)
-    file_list = list()
-    for diff in diff_list:
-        file_list.append(diff.a_path)
-    return file_list
-
-
-def is_file_path_change(file_list_a, file_list_b):
-    relative_path_list_a = set()
-    relative_path_list_b = set()
-    for path_a in file_list_a:
-        relative_path_list_a.add(path_a.replace(Values.PATH_A, ""))
-    for path_b in file_list_b:
-        relative_path_list_b.add(path_b.replace(Values.PATH_C, ""))
-    return not (relative_path_list_b == relative_path_list_a)
-
-
-# check if AST is covering all the changes (check for macros in play)
-def is_coverage(file_list_a, file_list_b):
-    for file_path in file_list_a:
-        source_line_numbers = get_source_line_numbers(file_path)
-        ast_line_numbers = get_ast_line_numbers(file_path)
-        if ast_line_numbers != source_line_numbers:
-            return False
-    for file_path in file_list_b:
-        source_line_numbers = get_source_line_numbers(file_path)
-        ast_line_numbers = get_ast_line_numbers(file_path)
-        if ast_line_numbers != source_line_numbers:
-            return False
-    return True
-
-
-def has_namespace_changed(file_list_a, file_list_b):
-    var_list_a = list()
-    var_list_b = list()
-    for file_path in file_list_a:
-        var_list = get_variable_list(file_path)
-        var_list_a = var_list_a + var_list
-    var_list_a = set(var_list_a)
-    for file_path in file_list_b:
-        var_list = get_variable_list(file_path)
-        var_list_b = var_list_b + var_list
-    var_list_b = set(var_list_b)
-    return not (var_list_a == var_list_b)
-
-
-def classify():
-    orig_file_list = get_diff_files(Values.PATH_B)
-    ported_file_list = get_diff_files(Values.PATH_E)
-    is_path_change = is_file_path_change(orig_file_list, ported_file_list)
-    is_covered = is_coverage(orig_file_list, ported_file_list)
-    is_translated = has_namespace_changed(orig_file_list, ported_file_list)
-
-
 def compare():
     Logger.trace(__name__ + ":" + sys._getframe().f_code.co_name, locals())
     global original_diff_info, ported_diff_info, transplanted_diff_info
-    Emitter.title("Ported Patch Analysis")
+    Emitter.title("Comparison with Manual Porting")
     load_values()
 
     if not Values.SKIP_ANALYSE:
         if not Values.PATH_E:
             error_exit("Path E is missing in configuration")
-        classify()
 
-        original_diff_info = safe_exec(analyse_source_diff, "analysing source diff of Original Patch",
-                                       Values.PATH_A, Values.PATH_B)
-        original_diff_info = safe_exec(analyse_ast_diff, "analysing ast diff of Original Patch",
-                                       Values.PATH_A, Values.PATH_B, original_diff_info)
         ported_diff_info = safe_exec(analyse_source_diff, "analysing source diff of Ported Patch",
                                      Values.PATH_C, Values.PATH_E)
         ported_diff_info = safe_exec(analyse_ast_diff, "analysing ast diff of Ported Patch",
                                      Values.PATH_C, Values.PATH_E, ported_diff_info)
 
-        if not Values.ONLY_ANALYSE:
-            transplanted_diff_info = safe_exec(analyse_source_diff, "analysing source diff of Transplanted Patch",
+        transplanted_diff_info = safe_exec(analyse_source_diff, "analysing source diff of Transplanted Patch",
                                          Values.PATH_C, Values.Project_D.path)
-            transplanted_diff_info = safe_exec(analyse_ast_diff, "analysing ast diff of Transplanted Patch",
+        transplanted_diff_info = safe_exec(analyse_ast_diff, "analysing ast diff of Transplanted Patch",
                                          Values.PATH_C, Values.Project_D.path, transplanted_diff_info)
+
+        print(ported_diff_info)
+        print(transplanted_diff_info)
 
         save_values()
     else:
