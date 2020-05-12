be72bb7bd9b90090f5c1602f105a70e12ad32dae
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2020-05-12 22:57:29 +0800

    add skip mapping option

--- phases/Initialization.py
+++ phases/Initialization.py
@@ -209,6 +209,17 @@ def read_conf():
                 Values.SKIP_TRANSLATION = True
                 Values.SKIP_DETECTION = True
                 Values.SKIP_SEGMENT = True
+            elif Definitions.ARG_ONLY_MAP in arg:
+                Values.ONLY_MAPPING = True
+                Values.SKIP_RESTORE = True
+                Values.SKIP_BUILD = True
+                Values.SKIP_WEAVE = True
+                Values.SKIP_DIFF = True
+                Values.SKIP_VERIFY = True
+                Values.SKIP_EXTRACTION = True
+                Values.SKIP_TRANSLATION = True
+                Values.SKIP_DETECTION = True
+                Values.SKIP_SEGMENT = True
             elif Definitions.ARG_ONLY_BUILD in arg:
                 Values.SKIP_VERIFY = True
                 Values.SKIP_WEAVE = True
