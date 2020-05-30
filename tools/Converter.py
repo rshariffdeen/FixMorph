8981ba4e49bad0bc40f9c2c74bb2780dd716d6d1
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2020-05-31 02:20:49 +0800

    print unhanhled node

--- tools/Converter.py
+++ tools/Converter.py
@@ -279,6 +279,7 @@ def convert_call_expr(ast_node, only_string=False):
                 continue
             operand_list.append(operand_var_name)
         else:
+            print(operand_node)
             error_exit("unhandled operand for call expr convert")
 
     var_name = function_name + "("
