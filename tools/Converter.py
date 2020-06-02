b6fc2261db17d4cd8435c5509f19c2987afece74
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2020-06-02 15:25:56 +0800

    return only string

--- tools/Converter.py
+++ tools/Converter.py
@@ -24,7 +24,7 @@ def convert_cast_expr(ast_node, only_string=False):
     param_node = ast_node['children'][1]
     param_node_type = param_node['type']
     if param_node_type == "MemberExpr":
-        param_node_var_name, param_node_data_type = convert_member_expr(param_node, True)
+        param_node_var_name = convert_member_expr(param_node, True)
         var_name = "(" + type_value + ") " + param_node_var_name + " " + var_name
     elif param_node_type == "DeclRefExpr":
         var_name = "(" + type_value + ") " + param_node['value'] + " " + var_name
@@ -36,11 +36,11 @@ def convert_cast_expr(ast_node, only_string=False):
         print(ast_node)
         error_exit("Unhandled CStyleCAST")
     if only_string:
-        return var_name, data_type
+        return var_name
     return var_name, var_list
 
 
-def convert_paren_node_to_expr(ast_node):
+def convert_paren_node_to_expr(ast_node, only_string=False):
     Logger.trace(__name__ + ":" + sys._getframe().f_code.co_name, locals())
     var_name = ""
     var_list = list()
@@ -49,18 +49,20 @@ def convert_paren_node_to_expr(ast_node):
     # print(child_node)
     child_node_type = child_node['type']
     if child_node_type == "BinaryOperator":
-        value, var_list = convert_binary_node_to_expr(child_node)
+        value = convert_binary_node_to_expr(child_node, True)
     elif child_node_type == "CStyleCastExpr":
-        value, var_list = convert_cast_expr(child_node)
+        value = convert_cast_expr(child_node, True)
     else:
         print(child_node)
         error_exit("Unknown child node type in parenexpr")
     var_name = "(" + value + ")"
     # print(var_name)
+    if only_string:
+        return var_name
     return var_name, list(set(var_list))
 
 
-def convert_unary_node_to_expr(ast_node):
+def convert_unary_node_to_expr(ast_node, only_string=False):
     Logger.trace(__name__ + ":" + sys._getframe().f_code.co_name, locals())
     var_name = ""
     var_list = list()
@@ -72,21 +74,23 @@ def convert_unary_node_to_expr(ast_node):
     if child_type in ["DeclRefExpr", "IntegerLiteral"]:
         child_value = str(child_node['value'])
     elif child_type == "BinaryOperator":
-        child_value, child_var_list = convert_binary_node_to_expr(child_node)
-        var_list = var_list + child_var_list
+        child_value = convert_binary_node_to_expr(child_node, True)
+        # var_list = var_list + child_var_list
     elif child_type == "MemberExpr":
-        child_value, child_data_type, child_var_list = convert_member_expr(child_node)
-        var_list = var_list + child_var_list
+        child_value = convert_member_expr(child_node, True)
+        # var_list = var_list + child_var_list
     elif child_type == "ParenExpr":
-        child_value, child_var_list = convert_paren_node_to_expr(child_node)
-        var_list = var_list + child_var_list
+        child_value = convert_paren_node_to_expr(child_node, True)
+        # var_list = var_list + child_var_list
     operation = str(ast_node['value'])
     # print(operation)
     var_name = child_value + operation
+    if only_string:
+        return var_name
     return var_name, list(set(var_list))
 
 
-def convert_binary_node_to_expr(ast_node):
+def convert_binary_node_to_expr(ast_node, only_string=False):
     Logger.trace(__name__ + ":" + sys._getframe().f_code.co_name, locals())
     var_name = ""
     var_list = list()
@@ -98,18 +102,18 @@ def convert_binary_node_to_expr(ast_node):
     if left_child_type in ["DeclRefExpr", "IntegerLiteral"]:
         left_child_value = str(left_child['value'])
     elif left_child_type == "BinaryOperator":
-        left_child_value, left_child_var_list = convert_binary_node_to_expr(left_child)
-        var_list = var_list + left_child_var_list
+        left_child_value = convert_binary_node_to_expr(left_child, True)
+        # var_list = var_list + left_child_var_list
     elif left_child_type == "ParenExpr":
-        left_child_value, left_child_var_list = convert_paren_node_to_expr(left_child)
-        var_list = var_list + left_child_var_list
+        left_child_value = convert_paren_node_to_expr(left_child, True)
+        # var_list = var_list + left_child_var_list
     elif left_child_type == "MemberExpr":
-        left_child_value, left_child_data_type, left_child_var_list = convert_member_expr(left_child)
-        var_list = var_list + left_child_var_list
+        left_child_value = convert_member_expr(left_child, True)
+        # var_list = var_list + left_child_var_list
     elif left_child_type == "Macro":
         lef_child_value = left_child['value']
     elif left_child_type == "CStyleCastExpr":
-        left_child_value, left_child_data_type, left_child_var_list = convert_cast_expr(left_child)
+        left_child_value = convert_cast_expr(left_child, True)
     else:
         print(left_child)
         error_exit("Unhandled child type in convert binary node")
@@ -122,27 +126,29 @@ def convert_binary_node_to_expr(ast_node):
     if right_child_type in ["DeclRefExpr", "IntegerLiteral"]:
         right_child_value = str(right_child['value'])
     elif right_child_type == "BinaryOperator":
-        right_child_value, right_child_var_list = convert_binary_node_to_expr(right_child)
-        var_list = var_list + right_child_var_list
+        right_child_value = convert_binary_node_to_expr(right_child, True)
+        # var_list = var_list + right_child_var_list
     elif right_child_type == "ParenExpr":
-        right_child_value, right_child_var_list = convert_paren_node_to_expr(right_child)
-        var_list = var_list + right_child_var_list
+        right_child_value = convert_paren_node_to_expr(right_child, True)
+        # var_list = var_list + right_child_var_list
     elif right_child_type == "MemberExpr":
-        right_child_value, right_child_data_type, right_child_var_list = convert_member_expr(right_child)
-        var_list = var_list + right_child_var_list
+        right_child_value = convert_member_expr(right_child, True)
+        # var_list = var_list + right_child_var_list
     elif right_child_type == "Macro":
         right_child_value = right_child['value']
         # var_list = var_list + right_child_var_list
     elif right_child_type == "CStyleCastExpr":
-        right_child_value, right_child_data_type, right_child_var_list = convert_cast_expr(right_child)
+        right_child_value = convert_cast_expr(right_child, True)
     else:
         print(right_child)
         error_exit("Unhandled child type in convert binary node")
     var_name = left_child_value + " " + operation + " " + right_child_value
+    if only_string:
+        return var_name
     return var_name, list(set(var_list))
 
 
-def convert_array_iterator(iterator_node):
+def convert_array_iterator(iterator_node, only_string=False):
     iterator_node_type = str(iterator_node['type'])
     var_list = list()
     if iterator_node_type in ["VarDecl", "ParmVarDecl"]:
@@ -166,16 +172,16 @@ def convert_array_iterator(iterator_node):
         iterator_value = str(iterator_node['value'])
         var_name = "[" + iterator_value + "]"
     elif iterator_node_type in ["BinaryOperator"]:
-        iterator_value, var_list = convert_binary_node_to_expr(iterator_node)
+        iterator_value = convert_binary_node_to_expr(iterator_node, True)
         var_name = "[" + iterator_value + "]"
     elif iterator_node_type in ["UnaryOperator"]:
-        iterator_value, var_list = convert_unary_node_to_expr(iterator_node)
+        iterator_value = convert_unary_node_to_expr(iterator_node, True)
         var_name = "[" + iterator_value + "]"
     elif iterator_node_type in ["MemberExpr"]:
-        iterator_value, iterator_type, var_list = convert_member_expr(iterator_node)
+        iterator_value = convert_member_expr(iterator_node, True)
         var_name = "[" + iterator_value + "]"
     elif iterator_node_type == "ParenExpr":
-        iterator_value, var_list = convert_paren_node_to_expr(iterator_node)
+        iterator_value = convert_paren_node_to_expr(iterator_node, True)
         var_name = "[" + iterator_value + "]"
     elif iterator_node_type == "CallExpr":
         iterator_value = convert_call_expr(iterator_node, True)
@@ -183,6 +189,8 @@ def convert_array_iterator(iterator_node):
     else:
         print(iterator_node)
         error_exit("Unknown iterator type for convert_array_iterator")
+    if only_string:
+        return var_name
     return var_name, var_list
 
 
@@ -202,7 +210,7 @@ def convert_array_subscript(ast_node, only_string=False):
         var_data_type = array_data_type.split("[")[0]
         iterator_node = ast_node['children'][1]
         iterator_node_type = str(iterator_node['type'])
-        iterator_name, var_list = convert_array_iterator(iterator_node)
+        iterator_name = convert_array_iterator(iterator_node, True)
         var_name = array_name + iterator_name
     elif array_type == "MemberExpr":
         array_name = str(array_node['value'])
@@ -210,27 +218,27 @@ def convert_array_subscript(ast_node, only_string=False):
         if 'data_type' in array_node.keys():
             array_data_type = str(array_node['data_type'])
         iterator_node = ast_node['children'][1]
-        array_name, array_data_type = convert_member_expr(array_node, True)
-        iterator_name, var_list = convert_array_iterator(iterator_node)
+        array_name = convert_member_expr(array_node, True)
+        iterator_name = convert_array_iterator(iterator_node, True)
         var_name = array_name + iterator_name
     elif array_type == "ParenExpr":
-        array_name, var_list = convert_paren_node_to_expr(array_node)
+        array_name = convert_paren_node_to_expr(array_node, True)
         var_data_type = None
         if 'data_type' in array_node.keys():
             var_data_type = str(array_node['data_type'])
         iterator_node = ast_node['children'][1]
-        iterator_name, var_list = convert_array_iterator(iterator_node)
+        iterator_name = convert_array_iterator(iterator_node, True)
         var_name = array_name + iterator_name
     elif array_type == "Macro":
         var_data_type = None
         iterator_node = ast_node['children'][1]
         array_name = str(array_node['value'])
-        iterator_name, var_list = convert_array_iterator(iterator_node)
+        iterator_name = convert_array_iterator(iterator_node, True)
         var_name = array_name + iterator_name
     elif array_type == "ArraySubscriptExpr":
-        array_name, var_data_type = convert_array_subscript(array_node, True)
+        array_name = convert_array_subscript(array_node, True)
         iterator_node = ast_node['children'][1]
-        iterator_name, var_list = convert_array_iterator(iterator_node)
+        iterator_name = convert_array_iterator(iterator_node, True)
         var_name = array_name + iterator_name
     else:
         print(array_type)
@@ -238,7 +246,7 @@ def convert_array_subscript(ast_node, only_string=False):
         print(ast_node)
         error_exit("Unknown data type for array_subscript")
     if only_string:
-        return var_name, var_data_type
+        return var_name
     return var_name, var_data_type, var_list
 
 
@@ -262,14 +270,14 @@ def convert_call_expr(ast_node, only_string=False):
         operand_node = ast_node['children'][i]
         operand_node_type = str(operand_node['type'])
         if operand_node_type == "CallExpr":
-            operand_var_name, operand_var_list = convert_call_expr(operand_node, only_string)
+            operand_var_name = convert_call_expr(operand_node, True)
             operand_list.append(operand_var_name)
-            var_list = var_list + operand_var_list
+            # var_list = var_list + operand_var_list
         elif operand_node_type == "DeclRefExpr":
             operand_var_name = str(operand_node['value'])
             operand_list.append(operand_var_name)
         elif operand_node_type == "MemberExpr":
-            operand_var_name, operand_data_type = convert_member_expr(operand_node, True)
+            operand_var_name = convert_member_expr(operand_node, True)
             operand_list.append(operand_var_name)
         elif operand_node_type == "Macro":
             operand_var_name = str(operand_node['value'])
@@ -325,12 +333,12 @@ def convert_member_expr(ast_node, only_string=False):
             # iterating_var_name = iterating_var_node['value']
             # iterating_var_type = iterating_var_node['type']
             # iterating_var_data_type = iterating_var_node['data_type']
-            iterating_var_name, iterating_var_list = convert_array_iterator(iterating_var_node)
+            iterating_var_name = convert_array_iterator(iterating_var_node, True)
 
             # if var_data_type == "":
             #     var_data_type = iterating_var_data_type
 
-            var_list = var_list + iterating_var_list
+            # var_list = var_list + iterating_var_list
             if var_name[:2] == "->":
                 var_name = "." + var_name[2:]
             var_name = iterating_var_name + var_name
@@ -347,9 +355,9 @@ def convert_member_expr(ast_node, only_string=False):
             param_node_var_name = ""
             param_node_aux_list = list()
             if param_node_type == "MemberExpr":
-                param_node_var_name, param_data_type = convert_member_expr(param_node, True)
+                param_node_var_name = convert_member_expr(param_node, True)
             elif param_node_type == "CStyleCastExpr":
-                param_node_var_name, param_data_type = convert_cast_expr(param_node, True)
+                param_node_var_name = convert_cast_expr(param_node, True)
             var_list = var_list + param_node_aux_list
             var_name = "(" + param_node_var_name + ")" + var_name
             break
@@ -366,8 +374,8 @@ def convert_member_expr(ast_node, only_string=False):
             else:
                 var_name = "->" + str(child_node_value.split(":")[-1]) + var_name
         elif child_node_type == "CallExpr":
-            child_var_name, child_aux_list = convert_call_expr(child_node)
-            var_list = var_list + child_aux_list
+            child_var_name = convert_call_expr(child_node, True)
+            # var_list = var_list + child_aux_list
             var_name = child_var_name + var_name
             break
 
@@ -379,7 +387,7 @@ def convert_member_expr(ast_node, only_string=False):
         else:
             child_node = None
     if only_string:
-        return var_name, var_data_type
+        return var_name
     return var_name, var_data_type, var_list
 
 
