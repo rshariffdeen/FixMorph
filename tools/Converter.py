2279a4dd947ae147b5049399b4d84df106cfed9a
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2020-06-06 13:55:44 +0800

    add declref to parenexp

--- tools/Converter.py
+++ tools/Converter.py
@@ -61,6 +61,8 @@ def convert_paren_node_to_expr(ast_node, only_string=False):
         value = child_node['value']
     elif child_node_type == "CallExpr":
         value = convert_call_expr(child_node, True)
+    elif child_node_type == "DeclRefExpr":
+        value = child_node['value']
     else:
         print(child_node)
         error_exit("Unknown child node type in parenexpr")
