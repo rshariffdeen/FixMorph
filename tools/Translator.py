5a6fb4411db69faaae7fa38b7eda17126eccff2d
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2020-06-14 22:09:38 +0800

    change move to replace

--- tools/Translator.py
+++ tools/Translator.py
@@ -722,7 +722,13 @@ def simplify_patch(instruction_AB, match_BA, ASTlists):
                         continue
             inserted.append(nodeB1)
             if nodeB2 not in inserted:
-                modified_AB.append((Definitions.MOVE, nodeB1, nodeB2, pos))
+                if nodeB2.type in ["UnaryOperator", "BinaryOperator"]:
+                    replace_node = nodeB2.children[pos]
+                    if replace_node.parent_id not in replaced:
+                        replaced.append(replace_node.id)
+                        modified_AB.append((Definitions.REPLACE, nodeB1, replace_node))
+                else:
+                    modified_AB.append((Definitions.MOVE, nodeB1, nodeB2, pos))
             else:
                 if nodeB2.type == "IfStmt":
                     continue
