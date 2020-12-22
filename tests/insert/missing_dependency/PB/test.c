376f5eab340e21fbd74ed663acbf23915389ed5d
rshariffdeen <rshariffdeen@gmail.com>
2020-12-22 15:30:09 +0800

    add new test case

---
+++ tests/insert/missing_dependency/PB/test.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>  
+
+void testLocalRefA(int a, char* str){
+    printf("the values are  %d %s\n", a, str);
+}
+
+void testGlobalRefRefA(){
+    printf("this is sample print\n");
+}
+
+void testGlobalRefA(char* str){
+     testGlobalRefRefA();
+     printf("the string is  %s\n", str);
+}
+
+int main( ) {
+    int i = 2;
+    testLocalRefA(i, "test");
+    return 0;
+}
---
+++ tests/insert/missing_dependency/PC/test.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>  
+
+void testLocalRefC(int a, char* str){
+     printf("the values are  %d %s\n", a, str);
+}
+
+void testGlobalRefRefC(){
+    printf("this is sample print\n");
+}
+
+void testGlobalRefC(char* str){
+     testGlobalRefRefC();
+     printf("the string is  %s\n", str);
+}
+
+int main( ) {
+    return 0;
+}
