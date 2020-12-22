4e51155f5a8ec2bc8fe252386383a283fd701632
rshariffdeen <rshariffdeen@gmail.com>
2020-12-22 16:40:20 +0800

    update test case

--- tests/insert/missing_dependency/PB/test.c
+++ tests/insert/missing_dependency/PB/test.c
@@ -6,14 +6,6 @@ void testLocalRefA(int a, char* str){
     printf("the values are  %d %s\n", a, str);
 }
 
-void testGlobalRefRef(){
-    printf("this is sample print\n");
-}
-
-void testGlobalRefA(char* str){
-     testGlobalRefRefA();
-     printf("the string is  %s\n", str);
-}
 
 int main( ) {
     int i = 2;
--- tests/insert/missing_dependency/PC/test.c
+++ tests/insert/missing_dependency/PC/test.c
@@ -6,10 +6,6 @@ void testLocalRefC(int a, char* str){
      printf("the values are  %d %s\n", a, str);
 }
 
-void testGlobalRefC(char* str){
-     testGlobalRefRefC();
-     printf("the string is  %s\n", str);
-}
 
 int main( ) {
     return 0;
