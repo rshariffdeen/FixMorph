fa17aa54fa9d68b99f6132a0fd618fb58d7b2c00
rshariffdeen <rshariffdeen@gmail.com>
2020-12-30 17:45:14 +0800

    add new test case

---
+++ tests/update/macro_function/PA/fib.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+#define SWAP(a,b) ({ a ^= b; b ^= a; a ^= b;})
+#define SQUARE(x) (x*x)
+#define TRACE_LOG(fmt, args...) fprintf(stdout, fmt, ##args);
+
+int fib(int n)
+{
+	if (n == 0 || n == 1)
+	{
+		return n;
+	}
+	return fib(n - 1) + fib(n - 2);
+}
+
+
+int main(void)
+{
+	int a = SQUARE(2);
+	scanf("%d", &a);
+	int fib_number = fib(a);
+	printf("fib number is %d", fib_number);
+	return 0;
+}
+
+
