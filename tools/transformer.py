b2c2153139d520a9506993d7d99daa1cf7790d9f
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2021-01-06 19:13:38 +0800

    use general arg in slice mode

--- tools/transformer.py
+++ tools/transformer.py
@@ -20,10 +20,10 @@ def transform_source_file(source_path, ast_script, output_file=None, ast_script_
     transform_command = definitions.PATCH_COMMAND + " -s=" + definitions.PATCH_SIZE
     if values.CONF_PATH_A in source_path or values.CONF_PATH_B in source_path:
         if values.DONOR_REQUIRE_MACRO:
-            transform_command += " " + values.DONOR_PRE_PROCESS_MACRO + " "
+            transform_command += " " + values.DONOR_PRE_PROCESS_MACRO.replace("--extra-arg-a", "--extra-arg") + " "
     if values.CONF_PATH_C in source_path or values.Project_D.path in source_path:
         if values.TARGET_REQUIRE_MACRO:
-            transform_command += " " + values.TARGET_PRE_PROCESS_MACRO + " "
+            transform_command += " " + values.TARGET_PRE_PROCESS_MACRO.replace("--extra-arg-c", "--extra-arg") + " "
 
     transform_command += " -script=" + ast_script_path + " -source=" + source_path + \
          " -destination=" + source_path + " -target=" + source_path + " -map=" + definitions.FILE_NAMESPACE_MAP_LOCAL
