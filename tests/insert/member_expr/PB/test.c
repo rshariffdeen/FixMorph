4c4ae674773100f69535124c3197b851911eccc8
rshariffdeen <rshariffdeen@gmail.com>
2021-01-07 21:09:35 +0800

    add new test case

---
+++ tests/insert/member_expr/PB/test.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>  
+
+struct regmap;
+struct snd_soc_component;
+struct sta32x_platform_data;
+
+
+/* codec private data */
+struct sta32x_priv {
+        struct regmap *regmap;
+        struct snd_soc_component *component;
+        struct sta32x_platform_data *pdata;
+        unsigned int mclk;
+        unsigned int format;
+        int coef_shadow[50];
+        int shutdown;
+
+};
+
+
+int test(struct snd_soc_component *component) {
+    struct sta32x_priv *sta32x;
+    sta32x->component = component;
+    return 0;
+}
+
+int main(){
+    return 0;
+}
---
+++ tests/insert/member_expr/PC/test.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>  
+
+struct regmap;
+struct snd_soc_codec;
+struct sta32x_platform_data;
+
+
+/* codec private data */
+struct sta32x_priv {
+        struct regmap *regmap;
+        struct snd_soc_codec *codec;
+        struct sta32x_platform_data *pdata;
+        unsigned int mclk;
+        unsigned int format;
+        int coef_shadow[50];
+        int shutdown;
+
+};
+
+
+int test(struct snd_soc_codec *codec) {
+    struct sta32x_priv *sta32x;
+    return 0;
+}
+
+int main(){
+    return 0;
+}
