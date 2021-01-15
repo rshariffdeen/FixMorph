04da9e4d71c89ec114691a184b00b30cdf3352af
rshariffdeen <rshariffdeen@gmail.com>
2021-01-15 17:16:19 +0800

    dont initialize if skipping

--- tools/parallel.py
+++ tools/parallel.py
@@ -100,11 +100,11 @@ def derive_namespace_map(ast_node_map, source_a, source_c, slice_file_a):
     for id_a, id_c, score, type_a, type_c in result_list:
         if id_a is None or id_c is None:
             continue
-        if id_a not in namespace_map:
-            namespace_map[id_a] = dict()
         if type_a in ["VarDecl", "ParmVarDecl", "RecordDecl", "FieldDecl"]:
             if score < 100:
                 continue
+        if id_a not in namespace_map:
+            namespace_map[id_a] = dict()
         if id_c not in namespace_map[id_a]:
             namespace_map[id_a][id_c] = score
         else:
