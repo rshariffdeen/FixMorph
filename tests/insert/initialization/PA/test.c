7215b71429a5af8b71e923d480bae46a327c572f
rshariffdeen <rshariffdeen@gmail.com>
2021-01-15 12:07:04 +0800

    add test case

---
+++ tests/insert/initialization/PA/test.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int fib(int n)
+{
+   int k;
+	if (n == 0 || n == 1)
+	{
+		return n;
+	}
+	return fib(n - 1) + fib(n - 2);
+}
+
+
+int main(void)
+{
+	int a;
+	scanf("%d", &a);
+	int fib_number = fib(a);
+	printf("fib number is %d", fib_number);
+	return 0;
+}
+
+
