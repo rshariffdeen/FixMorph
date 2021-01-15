29f31cefa7fca1cd75f344a4b1e51e50110471e1
rshariffdeen <rshariffdeen@gmail.com>
2021-01-15 15:13:09 +0800

    fix bug: update latest node after condition met

--- tools/evolver.py
+++ tools/evolver.py
@@ -260,10 +260,10 @@ def evolve_code(slice_file_list, source_file_list, instruction_list, seg_id_a, s
                 init_list = extractor.extract_initialization_node_list(scope_node)
                 latest_node = None
                 for node in init_list:
-                    latest_node = node
                     node_id = int(node['id'])
                     if node_id >= ref_node_id:
                         break
+                    latest_node = node
 
                 instruction = "Insert " + latest_node['type'] + "(" + str(latest_node['id']) + ")"
                 instruction += " into " + local_position_c
