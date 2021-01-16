e696e5b406ac870ba127308703c1f62abef22bbb
rshariffdeen <rshariffdeen@gmail.com>
2021-01-16 12:52:55 +0800

    add parent for callexpr

--- tools/filter.py
+++ tools/filter.py
@@ -351,6 +351,9 @@ def filter_namespace_map(namespace_map, edit_script, source_b):
         node_value = converter.get_node_value(node)
         if node_type in ["MemberExpr", "FieldDecl"]:
             node_value = "." + node_value
+        elif node_type == "DeclRefExpr":
+            if node['ref_type'] == "FunctionDecl":
+                node_value = node_value + "("
         if node_value:
             if node_value in namespace_map:
                 filtered_namespace_map[node_value] = namespace_map[node_value]
