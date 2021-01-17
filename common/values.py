902f4f2f84e89797da645a36f451b05f069818d0
rshariffdeen <rshariffdeen@gmail.com>
2021-01-17 18:22:24 +0800

    map goto statements

--- common/values.py
+++ common/values.py
@@ -106,7 +106,7 @@ file_transformation_info = dict()
 
 ast_map = dict()
 map_allow_type_list = [
-        "DeclRefExpr", "StringLiteral", "VarDecl", "MemberExpr", "Macro", "LabelStmt",
+        "DeclRefExpr", "StringLiteral", "VarDecl", "MemberExpr", "Macro", "LabelStmt", "GotoStmt",
         "ParmVarDecl", "RecordDecl", "FieldDecl", "FunctionDecl", "CallExpr"
     ]
 original_diff_info = dict()
