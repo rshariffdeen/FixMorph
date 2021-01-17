06f2c609bf9aae6af3f713540740631ec05b3158
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2021-01-18 01:40:05 +0800

    fix bug

--- tools/weaver.py
+++ tools/weaver.py
@@ -298,8 +298,8 @@ def weave_definitions(missing_definition_list, modified_source_list):
                 if "})" in transplant_code:
                     transplant_code = "#include<" + header_file.split("include/")[-1] + ">\n" + transplant_code
 
-                if transplant_code == "":
-                    transplant_code = "#define " + def_name + "(...)\n"
+                if "(" in def_name and transplant_code == "":
+                    transplant_code = "#define " + def_name + "...)\n"
         backup_file(target_file, FILENAME_BACKUP)
         insert_code(transplant_code, target_file, def_insert_line)
         if target_file not in modified_source_list:
