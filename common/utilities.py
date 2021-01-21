db2768198ccf1ebcafc549cae20bf75a5fafc33f
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2021-01-22 00:20:27 +0800

    add support for sydit

--- common/utilities.py
+++ common/utilities.py
@@ -9,6 +9,22 @@ from tools import logger, emitter, writer, reader
 from common import definitions, values
 
 
+def id_from_string(simplestring):
+    return int(simplestring.split("(")[-1][:-1])
+
+
+def get_id(node_ref):
+    return int(node_ref.split("(")[-1][:-1])
+
+
+def get_type(node_ref):
+    return node_ref.split("(")[0]
+
+
+def inst_comp(i):
+    return definitions.order.index(i)
+
+
 def execute_command(command, show_output=True):
     # Print executed command and execute it in console
     emitter.command(command)
@@ -246,34 +262,57 @@ def get_source_name_from_slice(slice_path):
     return source_path
 
 
+def shift_per_slice(slice_file):
+    vector_source = get_source_name_from_slice(slice_file)
+    backup_file_orig(vector_source)
+    replace_file(slice_file, vector_source)
+
+
 def shift_slice_source(slice_file_a, slice_file_c):
     values.current_slice_tuple = slice_file_a, slice_file_c
     slice_file_b = slice_file_a.replace(values.CONF_PATH_A, values.Project_B.path)
     slice_file_d = slice_file_c.replace(values.CONF_PATH_C, values.Project_D.path)
-    vector_source_a = get_source_name_from_slice(slice_file_a)
-    vector_source_b = vector_source_a.replace(values.CONF_PATH_A, values.Project_B.path)
-    vector_source_c = get_source_name_from_slice(slice_file_c)
-    vector_source_d = vector_source_c.replace(values.CONF_PATH_C, values.Project_D.path)
+    shift_per_slice(slice_file_a)
+    shift_per_slice(slice_file_b)
+    shift_per_slice(slice_file_c)
+    shift_per_slice(slice_file_d)
 
-    backup_file_orig(vector_source_a)
-    backup_file_orig(vector_source_b)
-    backup_file_orig(vector_source_c)
-    backup_file_orig(vector_source_d)
-    replace_file(slice_file_a, vector_source_a)
-    replace_file(slice_file_b, vector_source_b)
-    replace_file(slice_file_c, vector_source_c)
-    replace_file(slice_file_d, vector_source_d)
+    # vector_source_a = get_source_name_from_slice(slice_file_a)
+    # vector_source_b = vector_source_a.replace(values.CONF_PATH_A, values.Project_B.path)
+    # vector_source_c = get_source_name_from_slice(slice_file_c)
+    # vector_source_d = vector_source_c.replace(values.CONF_PATH_C, values.Project_D.path)
+    #
+    # backup_file_orig(vector_source_a)
+    # backup_file_orig(vector_source_b)
+    # backup_file_orig(vector_source_c)
+    # backup_file_orig(vector_source_d)
+    # replace_file(slice_file_a, vector_source_a)
+    # replace_file(slice_file_b, vector_source_b)
+    # replace_file(slice_file_c, vector_source_c)
+    # replace_file(slice_file_d, vector_source_d)
+
+
+def restore_per_slice(slice_file):
+    vector_source = get_source_name_from_slice(slice_file)
+    restore_file_orig(vector_source)
 
 
 def restore_slice_source():
     if values.current_slice_tuple:
         slice_file_a, slice_file_c = values.current_slice_tuple
-        vector_source_a = get_source_name_from_slice(slice_file_a)
-        vector_source_b = vector_source_a.replace(values.CONF_PATH_A, values.Project_B.path)
-        vector_source_c = get_source_name_from_slice(slice_file_c)
-        vector_source_d = vector_source_c.replace(values.CONF_PATH_C, values.Project_D.path)
+        slice_file_b = slice_file_a.replace(values.CONF_PATH_A, values.Project_B.path)
+        slice_file_d = slice_file_c.replace(values.CONF_PATH_C, values.Project_D.path)
+        restore_per_slice(slice_file_a)
+        restore_per_slice(slice_file_b)
+        restore_per_slice(slice_file_c)
+        restore_per_slice(slice_file_d)
 
-        restore_file_orig(vector_source_a)
-        restore_file_orig(vector_source_b)
-        restore_file_orig(vector_source_c)
-        restore_file_orig(vector_source_d)
+        # vector_source_a = get_source_name_from_slice(slice_file_a)
+        # vector_source_b = vector_source_a.replace(values.CONF_PATH_A, values.Project_B.path)
+        # vector_source_c = get_source_name_from_slice(slice_file_c)
+        # vector_source_d = vector_source_c.replace(values.CONF_PATH_C, values.Project_D.path)
+
+        # restore_file_orig(vector_source_a)
+        # restore_file_orig(vector_source_b)
+        # restore_file_orig(vector_source_c)
+        # restore_file_orig(vector_source_d)
