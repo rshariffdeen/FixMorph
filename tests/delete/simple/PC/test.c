dfdf47c0065cee9b7c46fa7cebb8de0f58c96887
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2021-04-25 03:23:56 +0800

    add new test case

---
+++ tests/delete/simple/PC/test.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int fib_n(int num)
+{
+	if (num == 0 || num == 1)
+	{
+		return num;
+	}
+	return fib_n(num - 1) + fib_n(num - 2);
+}
+
+
+int main(void)
+{
+	int k;
+	scanf("%d", &k);
+	int fn = fib_n(k);
+	k = printf("fib number is %d", fn);
+	if (k > 0){
+	    printf("TEST");
+	}
+	return 0;
+}
