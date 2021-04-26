217e58230689c1637b6d84ff2afaf5bc94eb8fac
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2021-04-26 14:43:59 +0800

    add test case

---
+++ tests/delete/if-condition/PA/test.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int fib(int n)
+{
+	if (n == 0 || n == 1)
+	{
+		return n;
+	}
+	return fib(n - 1) + fib(n - 2);
+}
+
+
+int main(void)
+{
+	int a;
+	scanf("%d", &a);
+	int fib_number = fib(a);
+	a = printf("fib number is %d", fib_number);
+	if (a > 0)
+	    printf("TEST");
+
+	return 0;
+}
+
+
