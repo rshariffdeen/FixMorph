4cf5d7b6a79b7702da6aa885c16ce030dc736089
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2021-04-27 04:18:46 +0800

    ignore update on replaced nodes

--- app/tools/translator.py
+++ app/tools/translator.py
@@ -229,6 +229,9 @@ def transform_script_gumtree(modified_script, inserted_node_list, json_ast_dump,
                 txt_update_node = instruction[2]
                 target_node = "?"
                 update_node_id = id_from_string(txt_update_node)
+                target_node_id = id_from_string(txt_target_node_a)
+                if target_node_id in replace_node_list_d:
+                    continue
                 if int(update_node_id) in update_list_d:
                     continue
                 update_node = json_ast_dump[values.Project_B.name][update_node_id]
