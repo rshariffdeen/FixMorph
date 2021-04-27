bee27c8505752cca0c05e2b3307a42c6964fb87f
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2021-04-28 01:54:48 +0800

    add new test case

---
+++ tests/special/api-call/PB/test.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>  
+
+void testGlobalRefA(){
+    printf("A: this is sample print\n");
+}
+
+void testGlobalRefB(){
+    printf("B: this is sample print\n");
+}
+
+
+void testLocalA(int a, char* str){
+    testGlobalRefB();
+    printf("the values are  %d %s\n", a, str);
+}
+
+void testLocalB(int a, char* str){
+    testGlobalRefB();
+    printf("the values are  %d %s\n", a, str);
+}
+
+void testLocalC(int a, char* str){
+    testGlobalRefB();
+    printf("the values are  %d %s\n", a, str);
+}
+
+
+int main( ) {
+    testLocalA(3, "testA");
+    testLocalB(3, "testB");
+    testLocalC(3, "testC");
+    return 0;
+}
---
+++ tests/special/api-call/PC/test.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>  
+
+void testGlobalRefX(){
+    printf("X: this is sample print\n");
+}
+
+void testGlobalRefY(){
+    printf("Y: this is sample print\n");
+}
+
+
+void testLocalX(int a, char* str){
+    testGlobalRefX();
+    printf("the values are  %d %s\n", a, str);
+}
+
+void testLocalY(int a, char* str){
+    testGlobalRefY();
+    printf("the values are  %d %s\n", a, str);
+}
+
+void testLocalZ(int a, char* str){
+    testGlobalRefY();
+    printf("the values are  %d %s\n", a, str);
+}
+
+
+int main( ) {
+    testLocalX(3, "testA");
+    testLocalY(3, "testB");
+    testLocalZ(3, "testC");
+    return 0;
+}
